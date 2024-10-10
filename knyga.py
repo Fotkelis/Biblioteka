d61c68fcd3af5da9bc680049ba36e796a7c97f31
Y********* <y*********@gmail.com>
2024-10-10 13:17:21 +0300

    Prideta knygos klase ir busena

---
+++ knyga.py
@@ -0,0 +1,16 @@
+class Knyga:
+    def __init__(self, autorius, pavadinimas, is_metai, zanras):
+        self.autorius = autorius
+        self.pavadinimas = pavadinimas
+        self.zanras = zanras
+        self.is_metai = is_metai 
+
+    def busena (self, ar_tinka):
+        if is_metai >= 1991:
+            print ("tinka")
+        else:
+            print("netinka, išmesti")
+
+        
+        
+    
