d2d4cb32a0b53acfd9d483783605442e9bab7b5e
Y********* <y*********@gmail.com>
2024-10-10 15:01:09 +0300

    inputas, kuris iraso i sarsas, veikiantis

--- nauja_knyga.py
+++ nauja_knyga.py
@@ -1,15 +1,16 @@
 
 nauja_knyga = []
 
-autorius = str()
-input("Įveskite knygos autorių")
+autorius = input("Įveskite knygos autorių")
+nauja_knyga.append(autorius)
 
-pavadinimas = str() 
-input("Įveskite knygos pavadinimą")
+pavadinimas = input("Įveskite knygos pavadinimą")
+nauja_knyga.append(pavadinimas)
 
-zanras = str()
-input("Įveskite knygos žanrą")
+zanras = input("Įveskite knygos žanrą")
+nauja_knyga.append(zanras)
 
-metai = int()
-input("Įveskite knygos išleidimo metus")
+metai = input("Įveskite knygos išleidimo metus")
+nauja_knyga.append(metai)
 
+print (nauja_knyga)
