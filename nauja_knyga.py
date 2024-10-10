875bc5f7d9a5659e27d0868e3f5cfb6355e92e98
Y********* <y*********@gmail.com>
2024-10-10 13:58:48 +0300

    vargas su inputais ir knygu sarasu

---
+++ nauja_knyga.py
@@ -0,0 +1,15 @@
+
+nauja_knyga = []
+
+autorius = str()
+input("Įveskite knygos autorių")
+
+pavadinimas = str() 
+input("Įveskite knygos pavadinimą")
+
+zanras = str()
+input("Įveskite knygos žanrą")
+
+metai = int()
+input("Įveskite knygos išleidimo metus")
+
