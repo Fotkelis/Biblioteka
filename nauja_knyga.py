c1f14b8822bc04ef54b69625610681ddad215f75
Y********* <y*********@gmail.com>
2024-10-14 19:20:39 +0300

    Saugojimas i faila ir pridejimas i zodyna neperrasant senu knygu, bet neveikia

--- nauja_knyga.py
+++ nauja_knyga.py
@@ -1,11 +1,12 @@
+import json
 knygos_nr = 0
    
 #Sarasas naujai knygai
     
 nauja_knyga = []
-
-def sukurti_knyga():
-    #kuriamas unikalus knygos numeris bibliotekoje
+while True:
+    
+#kuriamas unikalus knygos numeris bibliotekoje
     
     knygos_nr = knygos_nr + 1  
 
@@ -19,20 +20,26 @@ def sukurti_knyga():
 
     zanras = input("Įveskite knygos žanrą: ")
     nauja_knyga.append(zanras)
-
     metai = input("Įveskite knygos išleidimo metus: ")
     nauja_knyga.append(metai)
 
 #zodynas knygu bibliotekai
-visos_knygos = {}
-
-
+    visos_knygos = {}
 
 #vartotojo sukurta knyga pridedama i zodyna (biblioteka)
-visos_knygos[knygos_nr] = [nauja_knyga]   
+    visos_knygos[knygos_nr] = nauja_knyga
 
+# Klausimas, ar vartotojas nori pridėti kitą knygą
+    pasirinkimas = input("Ar norite pridėti kitą knygą? (taip/ne): ")
+    
+    if pasirinkimas.lower() != "taip":
+         
+    
+       break 
+    
 #spausdinama sukurta knyga ir visos knygos bibliotekoje
-print(f"Jūsų įvesta knyga {nauja_knyga} pridėta i biblioteka")
-print(f"Visos knygos {visos_knygos}")
+    print(f"Jūsų įvesta knyga {nauja_knyga} pridėta i biblioteka")
+    print(f"Visos knygos {visos_knygos}")
 
-
+with open('knygos_biblioteka.json', 'w', encoding='utf-8') as failas:
+    json.dump(visos_knygos, failas, ensure_ascii=False, indent=4)
