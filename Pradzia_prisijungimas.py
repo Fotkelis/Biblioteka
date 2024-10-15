e95cd524645fd46459564c84e6519536b9e965a0
Y********* <y*********@gmail.com>
2024-10-15 13:03:17 +0300

    tikrinam prisijungimus, jei tinka imortuojam knygos pridejima

--- Pradzia_prisijungimas.py
+++ Pradzia_prisijungimas.py
@@ -1,19 +1,23 @@
 username = "Paulius"
 passw = "1234567"
 
-user_in = input("Vartotojo vardas")
+user_in = input("Vartotojo vardas: ")
+passw_in = input("Slaptažodis: ")
 
-while True:
-    if user_in == username.lower(): 
-        print ("Sveiki atvykę, įveskite savo slaptažodį")
-    else:
-        if user_in != username:    
-            print ("Bandykite dar kartą")
-        else:
-            print ("Pabaiga")
+#tikrinam prisijungimus, jei tinka imortuojam knygos pridejima
 
-    
-    # pass_in = input("Slaptažodis")
+if user_in == username and passw == passw:
+    print("Užeikite")
+
+import nauja_knyga
+
+
+if user_in == username and passw != passw:
+    print("Neteisingas slaptažodis")
+if user_in != username and passw == passw:
+    print("Neteisingas vartotojo vardas")
+if user_in != username and passw != passw:
+    print("Neteisingas vartotojo vardas ir slaptažodis ")
 
 
 
